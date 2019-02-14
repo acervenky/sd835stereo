6f878a8d2450891ebfe19b288ed44cc338020202
Venkatesh Surve <acervenky@gmail.com>
2019-02-14 11:37:36 +0530

    Add files via upload

---
+++ common/.aml.sh
@@ -0,0 +1,28 @@
+RUNONCE=true
+
+patch_mixer_toplevel() {
+  case $1 in
+    -c) if [ "$(grep "<ctl name=\"$2\" value=\".*\" />" $MODPATH/system/vendor/etc/mixer_paths_tasha.xml)" ]; then
+          local num=$(sed -n "/<ctl name=\"$2\" value=\".*\" \/>/=" $MODPATH/system/vendor/etc/mixer_paths_tasha.xml | head -n1)
+          sed -i "$num s/\(<ctl name=\"$2\" value=\"\).*\(\" \/>\)/\1$3\2/" $MODPATH/system/vendor/etc/mixer_paths_tasha.xml
+        fi;;
+    -p) if [ "$(sed -n "/ *<path name=\"$2\">/,/ *<\/path>/ {/<ctl name=\"$3\" value=\".*\" \/>/p}" $MODPATH/system/vendor/etc/mixer_paths_tasha.xml)" ]; then
+          sed -i "/ *<path name=\"$2\">/,/ *<\/path>/ s/\(<ctl name=\"$3\" value=\".*\" \/>\)/\1\n        <ctl name=\"$4\" value=\"$5\" \/>/" $MODPATH/system/vendor/etc/mixer_paths_tasha.xml
+        fi;;
+  esac
+}
+
+patch_mixer_toplevel -c "SLIM RX0 MUX" "AIF_MIX1_PB"
+patch_mixer_toplevel -c "RX INT0_1 MIX1 INP0" "RX0"
+sed -i "/ *<path name=\"deep-buffer-playback quat_i2s\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia1\" value=\"1\" \/>\n\
+      <ctl name=\"RX0 HPF cut off\" value=\"MIN_3DB_150Hz\" \/>\n\
+      <ctl name=\"RX0 Digital Volume\" value=\"84\" \/>\n\
+      <ctl name=\"RX7 Digital Volume\" value=\"84\" \/>" $MODPATH/system/vendor/etc/mixer_paths_tasha.xml
+sed -i "/ *<path name=\"low-latency-playback quat_i2s\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia5\" value=\"1\" \/>\n\
+      <ctl name=\"RX0 HPF cut off\" value=\"MIN_3DB_150Hz\" \/>\n\
+      <ctl name=\"RX0 Digital Volume\" value=\"84\" \/>\n\
+      <ctl name=\"RX7 Digital Volume\" value=\"84\" \/>" $MODPATH/system/vendor/etc/mixer_paths_tasha.xml
+sed -i "/ *<path name=\"compress-offload-playback quat_i2s\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia4\" value=\"1\" \/>\n\
+      <ctl name=\"RX0 HPF cut off\" value=\"MIN_3DB_150Hz\" \/>\n\
+      <ctl name=\"RX0 Digital Volume\" value=\"84\" \/>\n\
+      <ctl name=\"RX7 Digital Volume\" value=\"84\" \/>" $MODPATH/system/vendor/etc/mixer_paths_tasha.xml
